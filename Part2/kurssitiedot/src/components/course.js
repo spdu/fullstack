db135010c69f2734426decf5ac556b9b7b7ff420
spdu <jmouru@gmail.com>
2019-11-20 23:40:48 +0200

    Part 2 tasks 2.1 - 2.5 completed

---
+++ Part2/kurssitiedot/src/components/course.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Course = ({course}) => {
+    return (
+      
+      <div>
+        {course.map((value, index) => { //Read through the course array and handle each course name separately
+          return <div key={index}>
+          <Header title={value.name} />
+          <Content course={value}/>
+          <Total parts={value.parts} />
+          </div>
+        })}
+      </div>
+    )
+    
+}
+
+const Header = ({title}) => <h1>{title}</h1>
+const Part = ({part}) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({course}) => {
+  //console.log("course in Content",course)
+    return (
+        <div>
+            {course.parts.map((value, index) => { return <div key={index}> <Part part={value} /> </div>})}
+        </div>
+    )
+}
+
+const Total = ({parts}) => {
+    //map the number of exercises
+    const totalExercises = parts.map(value => value.exercises)
+    const reducer = (accumulator, currentValue) => accumulator + currentValue //task 2.3*
+    const sum = totalExercises.reduce(reducer)
+    return (<b>Total of {sum} exercises</b>)
+}
+
+
+export default Course
--- Part2/kurssitiedot/src/index.js
+++ Part2/kurssitiedot/src/index.js
@@ -1,71 +1,55 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Course from './components/course'
 
-const Header = ({title}) => {
-    
-    return <h1>{title}</h1>
-
-}
-const Part = ({part}) => {
-return <div>{part.name} {part.exercises}</div>
-}
-
-const Content = ({course}) => {
-
-return (
-    <div>
-        <Part part={course.parts[0]} />
-        <Part part={course.parts[1]} />
-        <Part part={course.parts[2]} />
-    </div>  
-)
-}
-const Total = ({parts}) => {
-  const reducer = (accumulator, currentValue) => accumulator + currentValue
-
-  const sum = parts.reduce(reducer)
-  console.log('summa on:', sum.exercises)
-return (
-<p>Total of </p>
-)
-}
-const Course = ({course}) => {
-  console.log("Course name", course.name)
-  return (
-    <div>
-      <Header title={course.name} />
-      <Content course={course}/>
-      <Total parts={course.parts} />
-
-    </div>
-  )
-}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <Course course={courses}></Course>
     </div>
   )
 }
